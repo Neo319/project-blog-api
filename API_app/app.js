c2c9250ae777b3f8bd505e12588f3da556bef114
CastorDisaster <alexmnevins@gmail.com>
2024-10-23 14:13:34 -0400

    setup basic server structure

--- API_app/app.js
+++ API_app/app.js
@@ -1,1 +1,16 @@
-console.log("hello world!");
+require("dotenv").config();
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 2000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, (req, res) => {
+  console.log(`server listening on port ${port}`);
+});
