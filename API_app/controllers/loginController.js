3c14d8b669ae8cd7f36eddcefca36c76b17088c1
CastorDisaster <alexmnevins@gmail.com>
2024-10-24 14:18:14 -0400

    improve organization of login_controller

--- API_app/controllers/loginController.js
+++ API_app/controllers/loginController.js
@@ -8,8 +8,6 @@ const bcrypt = require("bcryptjs");
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-// ---- ROUTES ----
-
 // temp : mock database
 const users = [
   {
@@ -23,18 +21,17 @@ const users = [
 //temp: sample login
 // curl http://localhost:3000/api/login -X POST -H "Content-Type: application/json" -d '{"username": "Jane", "password": "asdf"}'
 
-// create JWT token and log in
+// ---- ROUTES ----
+// ----- create JWT token and log in -----
 const login_post = async (req, res) => {
   // getting credentials
   const { username, password } = req.body;
-
   // Find user
   const user = users.find((u) => u.username === username);
   if (!user) {
     console.log("User was not foud.");
     return res.status(401).send({ message: "User was not found!" });
   }
-
   // compare passwords
   const passwordIsValid = await bcrypt.compare(password, user.password);
   if (!passwordIsValid) {
@@ -42,54 +39,48 @@ const login_post = async (req, res) => {
     return res.status(401).send({ message: "Incorrect password!" });
   }
 
-  // authorization success: create JWT token
+  // --- authorization success: create JWT token ---
   const token = jwt.sign({ user: user }, SECRET_KEY, {
     // token options
     expiresIn: "10000s",
   });
-
+  // TODO: determine what to do with this token?
   res.json({
     message: "Login request success.",
     token: token,
   });
 };
 
-const signup_get = (req, res) => {
-  res.json({
-    message: "Signup GET request (NOT IMPLEMENTED)",
-  });
-};
-
+// ---- create new User in Database ----
 const signup_post = async (req, res) => {
-  // START HERE: implement passport, JWT
-
-  console.log("request:");
-  console.log(req.body);
+  const { username, password, email } = req.body;
 
   // tmp
   // sample request: (?)
-  // curl -X POST -H "Content-Type: application/json" -d '{"newUser": {"username": "first", "email": "first@gmail.com", "password": "fdsa"}}' http://localhost:3000/api/signup/
+  // curl -X POST -H "Content-Type: application/json" -d '{"username": "first", "email": "first@gmail.com", "password": "fdsa"}' http://localhost:3000/api/signup/
 
-  if (typeof req.body.newUser !== "undefined") {
-    try {
-      result = await prisma.user.create({
-        data: {
-          username: req.body.newUser.username,
-          email: req.body.newUser.email,
-          password: req.body.newUser.password,
+  if (!username || !password || !email) {
+    console.log("Error: incomplete credentials");
+    return res
+      .status(401)
+      .send({ message: "Error: missing signup credentials." });
+  }
 
-          isAuthor: false,
-        },
-      });
-      console.log("[debug]: created user in db.");
-      console.log(result);
-    } catch (err) {
-      console.log("signup err");
-      console.error(err.message);
-      res.status(400);
-    }
-  } else {
-    console.log("Error: no new user found");
+  try {
+    result = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: bcrypt.hashSync(req.body.newUser.password),
+
+        isAuthor: false,
+      },
+    });
+    console.log("[debug]: created user in db.");
+    console.log(result);
+  } catch (err) {
+    console.log("signup err");
+    console.error(err.message);
     res.status(400);
   }
 
