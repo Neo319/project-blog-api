08c2f591ce2ea3dc93cd182c05ed37a8cc54dd19
CastorDisaster <alexmnevins@gmail.com>
2024-10-24 15:00:43 -0400

    create sample db for posts

--- API_app/controllers/postsController.js
+++ API_app/controllers/postsController.js
@@ -2,6 +2,21 @@
 // const { PrismaClient } = require("@prisma/client");
 // const prisma = new PrismaClient();
 
+// temp: sample db
+const posts = [
+  {
+    id: 1,
+    title: "first",
+    date: Date.now(),
+
+    userId: 1,
+
+    isPublic: false,
+    textData:
+      '{"article" : "Hello! This is my first article. There should be a lot of text here."}',
+  },
+];
+
 const posts_get = (req, res) => {
   res.json({
     message: "GET posts request recieved (NOT IMPLEMENTED)",
