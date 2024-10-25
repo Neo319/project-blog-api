4f19503cb3b83e4f6d7e648b4f0d30906f796471
CastorDisaster <alexmnevins@gmail.com>
2024-10-25 13:27:30 -0400

    write base for upst put/delete functions

--- API_app/controllers/postsController.js
+++ API_app/controllers/postsController.js
@@ -80,6 +80,57 @@ const posts_post = [
   },
 ];
 
+//temp: sample req
+// curl -X POST -H "Authorization: Bearer >token<" -H "Content-Type: application/json" -d '{"authorId": "1", "title": "newPost", "text": "Hello! This is a sample post."}' http://localhost:3000/api/posts/
+
+// --- protected : POST new blog posts ---
+const posts_put = [
+  verify,
+  (req, res) => {
+    // temp: no db
+    const { authorId, title, text, isPublic } = req.body;
+
+    // ensure req has needed data
+    if (!authorId || !title || !text) {
+      console.log("missing data to post.");
+      return res.status(400).send({ message: "missing data to post." });
+    }
+
+    // ensure user is author
+    jwt.verify(req.token, SECRET_KEY, async (err, authData) => {
+      if (err) {
+        return res.status(403).send({ message: "error during authorization." });
+      }
+
+      const userIsAuthor = authData.user.isAuthor;
+      if (!userIsAuthor)
+        return res
+          .status(403)
+          .send({ message: "Forbidden: user is not author." });
+
+      // data is good, user is verified; post article
+      //temp: just return the article
+      // ------------- TODO: update to prisma here -------------
+      res.json({
+        message: "Authorized user + data and posted article.",
+        data: {
+          authorId,
+          title,
+          text,
+          isPublic: isPublic === "true" ? true : false,
+        },
+      });
+    });
+  },
+];
+
+const posts_delete = [
+  verify,
+  async (req, res) => {
+    res.send("deletion not implemented");
+  },
+];
+
 // TODO: implement PROTECTED ROUTES:
 // - Create, Update, Delete posts
 
