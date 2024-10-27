32b0d9660da54ab5c1363e37367abd04d6e59701
CastorDisaster <alexmnevins@gmail.com>
2024-10-27 13:24:47 -0400

    create initial http request (cors error)

---
+++ Author_app/tmp 1.05.39 PM/script.js
@@ -0,0 +1,17 @@
+const postList = document.getElementById("postList");
+
+const getPosts = async function () {
+  console.log("getPosts Runs");
+
+  try {
+    await fetch("http://localhost:3000/api", {
+      mode: "cors",
+    }).then((response) => {
+      console.log(response.json);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getPosts();
