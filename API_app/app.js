d04ef8c0978615ca7ace8761c7f0cea181053f59
CastorDisaster <alexmnevins@gmail.com>
2024-11-03 16:43:46 -0500

    successfully render post titles from user app

--- API_app/app.js
+++ API_app/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500", // Adjust origin if needed
+    origin: ["http://127.0.0.1:5500", "http://localhost:5173"], // Adjust origin if needed
     methods: "POST, GET, PUT, DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
--- User_app/user-blog-app/src/App.jsx
+++ User_app/user-blog-app/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// //getting posts
+// const { posts } = (async () => {
+//   const data = await fetch("http://localhost:3000/api/posts");
+//   const jsonData = await data.json();
+//   console.log(jsonData.posts);
+//   return jsonData.posts;
+// })();
 
 function App() {
-  const [count, setCount] = useState(0)
+  //getting posts
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/posts/");
+        const jsonData = await response.json();
+        console.log(jsonData.posts);
+        setPosts(Array.from(jsonData.posts));
+      } catch (err) {
+        console.error("error fetching posts", err);
+      }
+    };
+
+    // calls above defined function
+    fetchPosts();
+    // ensure effect is called only when page loads.
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Project Blog API</h1>
+
+      <h2>Posts List:</h2>
+
+      <ul>
+        <li>placeholder</li>
+        {posts ? (
+          posts.map((post, index) => {
+            return <li key={index}>{post.title}</li>;
+          })
+        ) : (
+          <li>Loading Posts...</li>
+        )}
+      </ul>
+
+      <footer>
+        <span>
+          Creator: Alex N.
+          <a target="blank" href="https://github.com/Neo319">
+            {" "}
+            (github)
+          </a>
+        </span>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
