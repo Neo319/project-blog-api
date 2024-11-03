83285d863fd2f03530285dba1473d56a9c097bc2
CastorDisaster <alexmnevins@gmail.com>
2024-11-03 17:09:06 -0500

    add description to App page

--- User_app/user-blog-app/src/App.jsx
+++ User_app/user-blog-app/src/App.jsx
@@ -7,10 +7,36 @@ function App() {
   //getting posts
   const { posts, loading } = useFetchPosts("http://localhost:3000/api/posts");
 
+  // TODO:
+  // -place posts list in a dropdown list
+  // -render main article or landing page using state
+  // -stylize article content
+
   return (
     <>
       <h1>Project Blog API</h1>
+      <p>
+        A part of The Odin Project Curriculum, this project integrates a backend
+        app that serves request made through this frontend app. Users can view
+        and comment on blog posts, and registered authors can create, read,
+        update, and delete blog posts.{" "}
+      </p>
+      <ul>
+        {/* TODO: place links in nav dropdown list */}
+        <li>
+          <a target="blank" href="https://theodinproject.com">
+            The Odin Project homepage
+          </a>
+        </li>
+        <a
+          target="blank"
+          href="https://www.theodinproject.com/lessons/node-path-nodejs-blog-api"
+        >
+          Link to relevant lesson
+        </a>
+      </ul>
 
+      {/* TEMP: list posts before placing in nav dropdown list */}
       <h2>Posts List:</h2>
 
       <ul>
@@ -23,6 +49,8 @@ function App() {
           })
         )}
       </ul>
+
+      {/* TODO: render selected post through state, or 'landing page' for blog */}
 
       <footer>
         <span>
